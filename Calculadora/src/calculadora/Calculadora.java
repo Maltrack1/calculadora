1f7c4d094ef215f8008d31e4c097935deb31929f
Rodrigo Reyes <kbryan_97@comunidad.unam.mx>
2017-04-24 13:26:07 -0500

    Multiplicacion

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -8,7 +8,11 @@ public class Calculadora {
         
     }
     public double multiplicar(){
-        
+        int x, y, result;
+        x = 5;
+        y = 6;
+        result = x* y;
+        System.out.println("Resultado = " + result");
     }
     public double dividir(){
         
