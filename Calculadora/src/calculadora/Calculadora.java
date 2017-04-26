d882260cd3ec72975b6691562bcf18b028b624e2
Nefta32 <nefta.gonza.27@gmail.com>
2017-04-26 17:02:09 -0500

    Update Calculadora.java

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,20 +1,33 @@
 package calculadora;
 
 public class Calculadora {
-    public double sumar(){
+     double x,y;
+    public calculadora(double x, double y){
+        this.x=x;
+        this.y=y;
         
+        double sum=x+y;
+        double res=x-y;
+        double multi=x*y;
+        double div=x/y;
+    }
+    
+    public double sumar(){
+        return sum;
     }
     public double restar(){
-        
+        return res;
     }
     public double multiplicar(){
-        int x, y, result;
+        /*int x, y, result;
         x = 5;
         y = 6;
         result = x* y;
         System.out.println("Resultado = " + result");
+       */
+        return multi;
     }
     public double dividir(){
-        
+        return div;
     }
 }
